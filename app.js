1dbc5d6275b2f46a95d9c319299b44c4c92047cf
majd eddine ben tahar <majdeddinebentahar@gmail.com>
2021-06-09 12:10:08 +0100

    lrsson 8

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-
 require("colors");
+
+const schema = require("./graphql/schema");
 
 const app = express();
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: "",
-    rootValue: "",
+    schema: schema,
   })
 );
 
