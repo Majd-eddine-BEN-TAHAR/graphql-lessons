bbd92a7eb44b2cadf3fd8609903c67d68cf7469c
majd eddine ben tahar <majdeddinebentahar@gmail.com>
2021-06-11 11:02:43 +0100

    lesson-10

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ const schema = require("./graphql/schema");
 
 const app = express();
 
+// localhost:3000/graphql => make graphql queries easily without any code
+
+// this is a get query using an id, it's like findById
+// query{
+//   book(id:"2"){
+//     name
+//   }
+// }
+
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
+    graphiql: true,
   })
 );
 
